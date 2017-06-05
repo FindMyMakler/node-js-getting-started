ce6eab5bcf2a6a8d846e356f8e6d37a9b51fd347
Srvizz <info@srvizz.com>
2017-06-05 22:07:10 +0100

    1
    1

--- index.js
+++ index.js
@@ -6,12 +6,3 @@ var layer = new LayerAPI({
   appId: 'layer:///apps/staging/2c96c436-44b0-11e7-9f2f-b79ffcf05b7b'
 });
 
-// Create a Conversation 
-layer.conversations.create({participants: ['abcd']}, function(err, res) {
-  var cid = res.body.id;
- 
-  // Send a Message 
-  layer.messages.sendTextFromUser(cid, 'abcd', 'Hello, World!', function(err, res) {
-    console.log(err || res.body);
-  });
-});
