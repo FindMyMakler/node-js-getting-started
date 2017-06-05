20b62924d2e40488c2ea9b879ad0905ad95a904e
Srvizz <info@srvizz.com>
2017-06-05 22:55:11 +0100

    1
    1

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var app = express();
 
 // Initialize by providing your Layer credentials 
 var layer = new LayerAPI({
-  token: API_TOKEN,
-  appId: APP_ID
+  token: "vnsELsfceQQBFo7jshxzbBOKmWOLxE4J6dNBR8V0q4XpfoZu",
+  appId: "layer:///apps/staging/2c96c436-44b0-11e7-9f2f-b79ffcf05b7b"
 });
 
 app.set('port', (process.env.PORT || 5000));
