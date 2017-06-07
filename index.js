0cf83dd96cffbbfabf3e68c4008ba17f744a55e9
Srvizz <info@srvizz.com>
2017-06-07 21:46:50 +0100

    1

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 var LayerWebhooks = require('layer-webhooks');
-
-// Initialize by providing your Layer credentials 
-var layer = new LayerWebhooks({
-    token: "vnsELsfceQQBFo7jshxzbBOKmWOLxE4J6dNBR8V0q4XpfoZu",
-    appId: "layer:///apps/staging/2c96c436-44b0-11e7-9f2f-b79ffcf05b7b"
-});
