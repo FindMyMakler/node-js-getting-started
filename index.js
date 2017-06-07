15eb6f2ba5fded8dc464ad6e32b909874043d871
Srvizz <info@srvizz.com>
2017-06-07 21:41:08 +0100

    1

--- index.js
+++ index.js
@@ -7,9 +7,3 @@ var layer = new LayerWebhooks({
     token: "vnsELsfceQQBFo7jshxzbBOKmWOLxE4J6dNBR8V0q4XpfoZu",
     appId: "layer:///apps/staging/2c96c436-44b0-11e7-9f2f-b79ffcf05b7b"
 });
-
-layer.webhooks.list(function(err, res) {
-  if (err) return console.error(err);
- 
-  var webhooks = res.body;
-});
